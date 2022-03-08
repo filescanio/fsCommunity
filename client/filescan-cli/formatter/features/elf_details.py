8cf09f671665403b26a7b74950569c9fa63f51a7
naidenko123 <andriinaidenko@yahoo.com>
2022-03-08 11:27:42 -0500

    feat: elf/mbox detail overview added

--- client/filescan-cli/formatter/features/elf_details.py
+++ client/filescan-cli/formatter/features/elf_details.py
@@ -1,5 +1,7 @@
 from typing import Dict
 from .base import BaseFormatter
+from common.colors import colorize
+from ..utils import format_dict
 
 
 class ElfFormatter(BaseFormatter):
@@ -13,3 +15,27 @@ class ElfFormatter(BaseFormatter):
         if self._get_short_type(report) != 'elf':
             return ''
 
+        result = f'''
+        {colorize('ELF Details')}
+        '''
+
+        overview_output = self.__format_overview(report)
+        result += overview_output
+        
+        return result
+
+
+    def __format_overview(self, report: Dict) -> str:
+        overview = self._get_details_overview(report)
+        extended = self._get_extended_data(report)
+
+        keys = []
+        for key in keys:
+            if key in extended:
+                overview[key] = extended[key]
+
+        overview_output = f'''
+            {colorize('Overview')}{format_dict(overview)}
+        '''
+
+        return overview_output
--- client/filescan-cli/formatter/features/mbox_details.py
+++ client/filescan-cli/formatter/features/mbox_details.py
@@ -1,5 +1,7 @@
 from typing import Dict
 from .base import BaseFormatter
+from common.colors import colorize
+from ..utils import format_dict
 
 
 class MboxFormatter(BaseFormatter):
@@ -13,3 +15,29 @@ class MboxFormatter(BaseFormatter):
         if self._get_short_type(report) != 'mbox':
             return ''
 
+        result = f'''
+        {colorize('Mbox Details')}
+        '''
+
+        overview_output = self.__format_overview(report)
+        result += overview_output
+        
+        return result
+
+
+    def __format_overview(self, report: Dict) -> str:
+        overview = self._get_details_overview(report)
+        resource = self._get_resource(report, 'file')
+
+        if 'metaData' in resource:
+            meta = resource['metaData']
+            keys = ['Subject', 'From', 'To', 'Date', 'In-Reply-To']
+            for key in keys:
+                if key in meta:
+                    overview[key] = meta[key]
+
+        overview_output = f'''
+            {colorize('Overview')}{format_dict(overview)}
+        '''
+
+        return overview_output
